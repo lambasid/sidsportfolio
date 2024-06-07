78c9fbc472d9a2e2af27de6e344a9ebe3879bf43
lambasid <lambasid@sheridancollege.ca>
2024-06-07 00:56:45 -0400

    feat: Add about, footer, header, contact, and projects components
    This commit adds the following components to the Angular project:
    - AboutComponent
    - FooterComponent
    - HeaderComponent
    - ContactComponent
    - ProjectsComponent
    
    These components were generated with their respective HTML and TypeScript files, along with their associated stylesheets. This commit provides the initial setup for these components, allowing further development and customization.
    
    Signed-off-by: lambasid <lambasid@sheridancollege.ca>

--- assignment2/src/app/app.module.ts
+++ assignment2/src/app/app.module.ts
@@ -5,14 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MaterialModule} from './module/material-ui.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
