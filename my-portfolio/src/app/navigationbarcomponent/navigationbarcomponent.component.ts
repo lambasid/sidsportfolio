067d78c52d8edac35eab4290f92501f110375dbc
lambasid <lambasid@sheridancollege.ca>
2024-06-18 18:43:30 -0400

    chore: Update npm dependencies and add styles for body and html elements
    Signed-off-by: lambasid <lambasid@sheridancollege.ca>

--- my-portfolio/src/app/navigationbarcomponent/navigationbarcomponent.component.ts
+++ my-portfolio/src/app/navigationbarcomponent/navigationbarcomponent.component.ts
@@ -1,6 +1,8 @@
 import { Component,Input } from '@angular/core';
 import {Personal,Projects} from '../interfacePersonalInfo';
 import personalInfoData from 'src/assets/data/personalInfo.json';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navigationbarcomponent',
